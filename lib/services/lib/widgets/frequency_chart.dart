1fe281b07f17013ee35495e497fce3e054aec8e5
POJOKAN NETWORK <139075790+pojokannetwork@users.noreply.github.com>
2025-11-01 00:52:14 +0700

    Update frequency_chart.dart

--- lib/services/lib/widgets/frequency_chart.dart
+++ lib/services/lib/widgets/frequency_chart.dart
@@ -1,1 +1,38 @@
 
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class FrequencyChart extends StatelessWidget {
+  final Map<String, int> data;
+  const FrequencyChart({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    final entries = data.entries.toList()
+      ..sort((a, b) => a.key.compareTo(b.key));
+
+    final series = [
+      charts.Series<MapEntry<String, int>, String>(
+        id: 'DigitFreq',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (e, _) => e.key,
+        measureFn: (e, _) => e.value,
+        data: entries,
+        labelAccessorFn: (e, _) => '${e.value}',
+      )
+    ];
+
+    return SizedBox(
+      height: 250,
+      child: charts.BarChart(
+        series,
+        animate: true,
+        behaviors: [
+          charts.ChartTitle('Frekuensi Digit 0–9'),
+          charts.BarLabelDecorator<String>(),
+        ],
+        domainAxis: const charts.OrdinalAxisSpec(),
+      ),
+    );
+  }
+}
